cf4b3eb6bb5f5e86249bfb33b4302996e4abd3ef
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-03-18 17:14:19 +0100

    added concurrently dependency for running both server and client with a single command

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ app.use('/api/users', userRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/posts', postRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on ${port}`));
